fec7040aa22dfec2712c355449961f3a8ed10b03
Kyle Burt <kyle2burt@gmail.com>
2023-03-31 13:07:10 -0700

    Add getComputerChoice function
    randomly returns either rock, paper, or scissors by generating a random
    number between 0 and 2 and assigning a value based on that random
    number

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,1 +1,20 @@
 console.log("Rock Paper Scissors!");
+
+function getComputerChoice() { // Randomly return rock, paper, or scissors
+    let computerChoice
+    let randomNumber = Math.floor(Math.random() * 3); // Random number between 0 and 2
+
+    switch (randomNumber) {
+        case 0:
+            computerChoice = "Rock"
+            break;
+        case 1:
+            computerChoice = "Paper"
+            break;
+        case 2:
+            computerChoice = "Scissors"
+            break;
+    }
+
+    return computerChoice;
+}
